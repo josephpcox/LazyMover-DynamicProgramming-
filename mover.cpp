cdf5e0b930ac1b9d1ccab939e9ac21bcac6878fb
AdrianB1995 <aborrego1@mail.usf.edu>
2018-10-30 02:58:49 -0400

    Main file

---
+++ mover.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <vector>
+#include <stack>
+#include <fstream>
+#include <mover.h>
+#include <sstream> //std::istringstream
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // ifstream readFrom;
+    // ofstream writeTo;
+    // readFrom.open("input.txt");
+    // writeTo.open("output.txt");
+    // if(!readFrom.is_open()))
+    //     perror("Error while opening input.txt file for reading.\n");
+    // if(!writeTo.is_open())
+    //     perror("Error while opening output.txt file for writing.\n");
+
+    myMover = Mover();
+    currentCount = 0;
+    maxCount = 0;
+
+    for(i = 0; i < myMover.boxList.size(); i++)
+    {
+        if(myMover.boxList[i] > myMover.currentBox.top() && MatrixRow >= myMover.boxList[i]){
+            myMover.currentBox.pop();
+            currentCount -= 1;
+            myMover.currentBox.push(boxList[i]);
+            currentCount += 1;
+        }
+        else{
+            myMover.currentBox.push(boxList[i]);
+            currentCount += 1;
+        }
+        maxCount = currentCount;
+    }
+
+    //Closing files
+    // readFrom.close();
+    // writeTo.close();
+    return 0;
+
+}
+

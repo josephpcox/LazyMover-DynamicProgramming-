fafefb18687d94b5a2d18e5f5d8b068ad11672e1
Joseph Cox <josephcox@USF-Gold-Wifi-226-5-122.laptops.usf.edu>
2018-10-30 22:24:30 -0400

    made the shell for the main it reades in the input.txt

--- mover.cpp
+++ mover.cpp
@@ -1,43 +1,59 @@
-#include <stdlib.h>
-#include <vector>
-#include <stack>
+#include <iostream>
 #include <fstream>
-#include "Mover.hpp"
-#include <sstream> //std::istringstream
-
+#include <vector>
+#include <string>
+#include <sstream> 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    // ifstream readFrom;
-    // ofstream writeTo;
-    // readFrom.open("input.txt");
-    // writeTo.open("output.txt");
-    // if(!readFrom.is_open()))
-    //     perror("Error while opening input.txt file for reading.\n");
-    // if(!writeTo.is_open())
-    //     perror("Error while opening output.txt file for writing.\n");
+/*struct Move{
+    vector <int> dolly;
+    
+    // Constructor
+    Mover(){
+        
+    }
+};
+*/
 
-    Mover myMover = Mover(5);
-    for(int i = 0; i <= myMover.boxList.size(); i++)
-    {
-        if(myMover.boxList[i] > myMover.currentBox.top() && 1 >= myMover.boxList[i]){
-            myMover.currentBox.pop();
-            myMover.currentCount -= 1;
-            myMover.currentBox.push(myMover.boxList[i]);
-            myMover.currentCount += 1;
+int main (){
+    // make sure that the files can be read and instreamed 
+    /***************************************************************************/
+    ifstream input_file("input.txt"); // open the input file
+    if (!input_file.is_open()) { // check for successful opening
+        cout << "Input file could not be opened! Terminating!" << endl;
+        return 1;
+    }
+    ofstream output_file("output.txt"); // open the output file
+    if (!output_file.is_open()) { // check for successful opening
+        cout << "Output file could not be opened! Terminating!" << endl;
+        return 1;
+    }
+    /***************************************************************************/
+
+    // read in the stream until the EOF
+    stringstream ss;
+    string line;
+    bool firstTime = true;
+    int numObjects=0;
+    while (input_file) {
+        // we need to know how many objects need to be created 
+        if(firstTime == true){
+            getline(input_file,line);
+            ss << line;
+            ss >> numObjects;
+            cout<<"This is the number of objects that need to be created for this file "<<numObjects<<endl;
+            firstTime = false;
         }
-        else{
-            myMover.currentBox.push(myMover.boxList[i]);
-            myMover.currentCount += 1;
-        }
-        myMover.maxCount = myMover.currentCount;
+        // Read a Line from File 
+        getline(input_file, line); 
+        // Print line in Console to make sure the line is correct 
+        cout << line << endl; 
     }
 
-    //Closing files
-    // readFrom.close();
-    // writeTo.close();
+
+    // make sure we close the files when we are finished
+    input_file.close();
+    output_file.close();
+    cout << "Done!" << endl;
     return 0;
-
 }
-
--- moverImp.cpp
+++ moverImp.cpp
@@ -4,28 +4,59 @@
 *******************************************/
 #include "Mover.hpp"
 
-using namespace std;
 /*contructor to build the mover class object
  * Initialize the vector with the size and initilize the empty stack
  */
-Mover::Mover (int size){
-  boxList.resize(size);
+Mover::Mover (int size,vector<int>& list){
+    boxList = list;
+    size = size;
 }
 
 //Getters/Setters for maxCount
 int Mover::getMaxCount() const{
-    return this->maxCount;
+    return maxCount;
 }
 
 void Mover:: setMaxCount(int newCount){
-    this->maxCount = newCount;
+    maxCount = newCount;
 }
 
 //Getters/Setters for currentCount
 int Mover:: getCurrentCount() const{
-    return this->currentCount;
+    return currentCount;
 }
 
 void Mover::setCurrentCount(int newCount){
-    this->currentCount = newCount;
+    currentCount = newCount;
 }
+int Mover::findGreatestBox(map<int,int>& Store, vector<int> boxList&){
+    if(Store.find(key)==Store.end()){
+        cout<<"Box Does not exist iterating through list now"<<endl;
+        int i = boxList.size();
+        int count = 0;
+        while(i>0){
+            // for debugging
+            if(boxList.size()==0){
+                cout<<"Error: list size is zero, list does not exist"<<endl;
+                return -1;
+            }
+            // base case is list size is equal to 1
+            else if(boxList.size()==1){
+                count++;
+                Store.insert(i,count);
+                return count;
+
+            }
+            else{
+
+            }
+            i--;
+        }
+    }
+    else{
+        return Store.find(key);
+    }
+
+     
+}
+
